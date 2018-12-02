258eb686807f7c2b410e7c49ebfd3f1718de57a4
Oleksii Pidgornyi <oleksii.pidgornyi@gmail.com>
2018-12-02 22:58:30 +0100

    CALayer project created

---
+++ CALayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CALayer
+//
+//  Created by Oleksii Pidgornyi on 12/2/18.
+//  Copyright © 2018 Oleksii Pidgornyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
